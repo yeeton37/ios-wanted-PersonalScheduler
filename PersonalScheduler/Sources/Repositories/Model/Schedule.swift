67fa02f70d5fe9192cfcbd8c0d5823b5602230d4
yeeton37 <lyeeun37@naver.com>
2023-01-12 17:14:31 +0900

    feat: Schedule 모델 생성

---
+++ PersonalScheduler/Sources/Repositories/Model/Schedule.swift
@@ -0,0 +1,15 @@
+//
+//  Schedule.swift
+//  PersonalScheduler
+//
+//  Created by 이예은 on 2023/01/12.
+//
+import Foundation
+
+struct Schedule {
+    let id: UUID
+    let title: String
+    let body: String
+    let startDate: String
+    let endDate: String
+}
