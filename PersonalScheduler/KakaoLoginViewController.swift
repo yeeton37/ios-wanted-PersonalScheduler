5f4e96c983374dd97a0c88939407ccc6e9b97786
yeeton37 <lyeeun37@naver.com>
2023-01-12 15:26:04 +0900

    feat: 코코아팟을 통해 KakaoSDK 라이브러리 설치

---
+++ PersonalScheduler/KakaoLoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KakaoLoginViewController.swift
+//  PersonalScheduler
+//
+//  Created by 이예은 on 2023/01/11.
+//
+
+import UIKit
+
+class KakaoLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
