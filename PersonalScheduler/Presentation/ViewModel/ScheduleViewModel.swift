7478158bddde22463c448ad257545a8272c2ea21
yeeton37 <lyeeun37@naver.com>
2023-01-12 23:57:34 +0900

    feat: 스케줄을 보여주는 화면과 ViewModel 구현

--- PersonalScheduler/Presentation/ViewModel/ScheduleViewModel.swift
+++ PersonalScheduler/Presentation/ViewModel/ScheduleViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Combine
+
+class ScheduleViewModel {
+    var model: [Schedule]?
+    var _model = PassthroughSubject<[Schedule], Never>()
+    var email: String
+    
+    init(email: String) {
+        self.email = email
+    }
+    
+    func viewWillAppear() {
+        FirebaseManager.shared.fetchAll { [weak self] schedules in
+            if let self = self {
+                var schedule = schedules.filter {
+                    $0.email == self.email
+                }
+                
+                self.model = schedule
+                
+                self._model.send(schedule)
+            }
+        }
+    }
+}
