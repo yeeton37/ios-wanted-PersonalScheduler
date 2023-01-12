c5bf7094c21c8933252915c3994b5ff25d35c8bc
yeeton37 <lyeeun37@naver.com>
2023-01-12 17:07:41 +0900

    feat: 카카오 로그인 시 파이어베이스 유저 생성 기능 추가

--- PersonalScheduler/KakaoLoginViewController.swift
+++ PersonalScheduler/KakaoLoginViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import KakaoSDKUser
+import FirebaseAuth
 
 class KakaoLoginViewController: UIViewController {
     
@@ -22,9 +23,29 @@ class KakaoLoginViewController: UIViewController {
                 
                 //do something
                 _ = oauthToken
+                
+                // 로그인 성공 시
+                UserApi.shared.me { kuser, error in
+                    if let error = error {
+                        print("------KAKAO : user loading failed------")
+                        print(error)
+                    } else {
+                        // 파이어베이스 유저 생성 (이메일로 회원가입)
+                        Auth.auth().createUser(withEmail: (kuser?.kakaoAccount?.email)!, password: "\(String(describing: kuser?.id))") { result, error in
+                            if let error = error {
+                                print("파이어베이스 사용자 생성 실패 \(error.localizedDescription)")
+                                
+                                Auth.auth().signIn(withEmail: (kuser?.kakaoAccount?.email)!, password: "\(String(describing: kuser?.id))", completion: nil)
+                            } else {
+                                print("파이어베이스 사용자 생성")
+                                let vc = SchedularViewController()
+                                self.show(vc, sender: nil)
+                            }
+                        }
+                    }
+                }
             }
             
-            self.navigationController?.popViewController(animated: true)
         }
     }
 }
